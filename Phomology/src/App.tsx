a7c798de2e62c466ead13e28e3471a1ffb179490
UlissesNunes <ulissesnunesx@gmail.com>
2026-02-18 12:27:06 -0300

    update

--- Phomology/src/App.tsx
+++ Phomology/src/App.tsx
@@ -1,12 +1,29 @@
 
+import type { TaskStateModel } from "./Models/TaskStateModel";
+
+import { useState } from 'react';
 
 
+import { Home } from './Pages/Home';
 
-import { SobrePhomo } from './Pages/SobrePhomo';
+const InitialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 35,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 
 
 export function App() {
-  return <SobrePhomo/>
+
+  const [state, setState] = useState(InitialState);
+  return <Home state={state} setState={setState}/>
   
 }
--- Phomology/src/Pages/Home/index.tsx
+++ Phomology/src/Pages/Home/index.tsx
@@ -1,8 +1,15 @@
 import CounterPhomo from "../../../componentes/CounterPhomo";
 import FormMainPhomo from "../../../componentes/FormMainPhomo";
+import type { TaskStateModel } from "../../Models/TaskStateModel";
 import { MainTemplates } from "../../templates/MainTemplates";
 
-export function Home () {
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+}
+
+export function Home (Props: HomeProps) {
+  console.log(Props);
   return (
     <>
     <MainTemplates>
